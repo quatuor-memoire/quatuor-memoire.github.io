bbcea0749bef521311c2b18c827b05bbd0af9221
Itai Epstein <44759817+itaie01@users.noreply.github.com>
2025-10-23 20:05:49 +0200

    about page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  i18n: {
+    locales: ["en", "fr"],
+    defaultLocale: "en",
+    routing: {
+      prefixDefaultLocale: true
+    },
+  },
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
