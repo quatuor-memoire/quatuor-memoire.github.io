495f6b3905c5b7f83d84f7dde22dd36b3ff54b87
Itai Epstein <44759817+itaie01@users.noreply.github.com>
2025-11-21 16:37:15 +0100

    domain stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://quatuor-memoire.github.io",
+  site: "https://www.quatuormemoire.com",
   i18n: {
     locales: ["en", "fr"],
     defaultLocale: "en",
